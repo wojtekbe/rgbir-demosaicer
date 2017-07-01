70b73555616a9d5371a6c85431794bd951345715
Wojciech Bieganski <wojtekbe@gmail.com>
2017-07-01 14:49:00 +0200

    Migrate to opencv 3.1.0

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include "opencv2/opencv.hpp"
-#include "opencv2/gpu/gpu.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/cudaarithm.hpp>
 
 int main (int argc, char* argv[])
 {
     try
     {
-        cv::Mat src_host = cv::imread("file.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-        cv::gpu::GpuMat dst, src;
+        cv::Mat src_host = cv::imread("file.jpg", cv::IMREAD_GRAYSCALE);
+        cv::cuda::GpuMat dst, src;
         src.upload(src_host);
 
-        cv::gpu::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
+        cv::cuda::threshold(src, dst, 128.0, 255.0, cv::THRESH_BINARY);
 
         cv::Mat result_host;
         dst.download(result_host);
