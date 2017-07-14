4cc9c050072afc9ef3f301fd370fc27a2a7f89b2
Wojciech Bieganski <wojtekbe@gmail.com>
2017-07-14 15:44:44 +0200

    Fix indents

--- demosaicer.cpp
+++ demosaicer.cpp
@@ -32,28 +32,28 @@ cv::Mat load_raw(const char *fname, int size)
 
 int main (int argc, char* argv[])
 {
-    try
-    {
+	try
+	{
 		cv::Mat img_raw = load_raw(argv[1], IMG_W*IMG_H*2);
 		cv::Mat src_host8;
 		img_raw.convertTo(src_host8, CV_8UC1, 255.0/1023.0);
 
-        cv::cuda::GpuMat dst, src;
-        src.upload(src_host8);
+		cv::cuda::GpuMat dst, src;
+		src.upload(src_host8);
 
-        cv::cuda::demosaicing(src, dst, cv::cuda::COLOR_BayerRG2RGB_MHT);
+		cv::cuda::demosaicing(src, dst, cv::cuda::COLOR_BayerRG2RGB_MHT);
 
-        cv::Mat result_host;
-        dst.download(result_host);
+		cv::Mat result_host;
+		dst.download(result_host);
 
-        cv::imwrite("ifile.png", src_host8);
-        cv::imwrite("ofile.png", result_host);
-    }
+		cv::imwrite("ifile.png", src_host8);
+		cv::imwrite("ofile.png", result_host);
+	}
 
-    catch(const cv::Exception& ex)
-    {
-        cout << "Error: " << ex.what() << endl;
-    }
+	catch(const cv::Exception& ex)
+	{
+		cout << "Error: " << ex.what() << endl;
+	}
 
-    return 0;
+	return 0;
 }
