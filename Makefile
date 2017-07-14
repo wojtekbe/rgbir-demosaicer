ad61e301bd58fdabf7cb12c01c24d7e3c37487ee
Wojciech Bieganski <wojtekbe@gmail.com>
2017-07-14 15:40:16 +0200

    OpenCV raw demosaicer (RGB pattern)

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 CXXFLAGS=`pkg-config --cflags opencv`
-#LDFLAGS=-g -L/usr/lib -lopencv_calib3d -lopencv_contrib -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_gpu -lopencv_highgui -lopencv_imgproc -lopencv_legacy -lopencv_ml -lopencv_objdetect -lopencv_photo -lopencv_stitching -lopencv_superres -lopencv_video -lopencv_videostab -lopencv_esm_panorama -lopencv_facedetect -lopencv_imuvstab -lopencv_tegra -lopencv_vstab -L/usr/local/cuda/lib -lcufft -lnpps -lnppi -lnppc -lcudart -lrt -lpthread -lm -ldl
 LDFLAGS=`pkg-config --libs opencv`
 
-main: main.cpp
+demosaicer: demosaicer.cpp
 	$(CXX) $< $(CXXFLAGS) $(LDFLAGS) -o $@
 
 clean:
-	rm -rf main *.o
+	rm -rf demosaicer *.o
---
+++ demosaicer.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <stdio.h>
+
+using namespace std;
+
+#define IMG_W 1920
+#define IMG_H 1080
+
+cv::Mat load_raw(const char *fname, int size)
+{
+	uchar* raw_data;
+	FILE *fp;
+	int i;
+
+	raw_data = (uchar *)malloc(size);
+
+	fp = fopen(fname, "rb");
+	if (fp != NULL) {
+		for (i = 0; i < size; i++) {
+			raw_data[i] = getc(fp);
+		}
+	}
+	fclose(fp);
+	cv::Mat im(IMG_H, IMG_W, CV_16UC1, raw_data);
+
+	return im;
+}
+
+int main (int argc, char* argv[])
+{
+    try
+    {
+		cv::Mat img_raw = load_raw(argv[1], IMG_W*IMG_H*2);
+		cv::Mat src_host8;
+		img_raw.convertTo(src_host8, CV_8UC1, 255.0/1023.0);
+
+        cv::cuda::GpuMat dst, src;
+        src.upload(src_host8);
+
+        cv::cuda::demosaicing(src, dst, cv::cuda::COLOR_BayerRG2RGB_MHT);
+
+        cv::Mat result_host;
+        dst.download(result_host);
+
+        cv::imwrite("ifile.png", src_host8);
+        cv::imwrite("ofile.png", result_host);
+    }
+
+    catch(const cv::Exception& ex)
+    {
+        cout << "Error: " << ex.what() << endl;
+    }
+
+    return 0;
+}
